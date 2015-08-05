03872c0cbf98942ee25af4009ba6520fd3c90ec4
Ricky Joudrey <rjoudrey@users.noreply.github.com>
2015-08-05 12:04:49 -0400

    Change main view controller to show cats

---
+++ cats/CatCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CatCollectionViewCell.swift
+//  cats
+//
+//  Created by Ricky J on 8/5/15.
+//  Copyright © 2015 rjoudrey. All rights reserved.
+//
+
+import UIKit
+
+class CatCollectionViewCell: UICollectionViewCell {
+    @IBOutlet var catImageView : UIImageView?
+}
